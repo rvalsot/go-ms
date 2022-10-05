21a9713246337f59231b72e482fce1769a45fcd9
rvalsot <rene.valsot@gmail.com>
2022-10-05 14:23:25 -0500

    pushing 2.5 chapter advances to github

---
+++ handlers/hello.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+	loog *log.Logger
+}
+
+func NewHellow(loog *log.Logger) *Hello {
+	return &Hello{loog}
+}
+
+func (h *Hello) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	h.loog.Println("Hellow wourld!")
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(rw, "Ooopsy, a request error ocurred", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(rw, "Hello, %s", data)
+
+}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -11,19 +10,7 @@ func main() {
 	fmt.Println("Server up & running at port 9090")
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello from your sever!")
-		dataRead, err := ioutil.ReadAll(r.Body)
-
-		if err != nil {
-			http.Error(rw, "There was an Oopsy", http.StatusBadRequest)
-			return
-		}
-
-		fmt.Printf("Did you say? %s \n", dataRead)
-
-		if string(dataRead) == "Ping" {
-			fmt.Println("Server says: Pong!")
-		}
+		helloHandler := handlers.NewHellow()
 	})
 
 	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
